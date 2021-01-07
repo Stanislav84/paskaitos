6cc530352b7486814fe2dc52fe21f261e05459f0
Stanislav84 <76004794+Stanislav84@users.noreply.github.com>
2021-01-07 15:42:43 +0200

    Kintamieji

---
+++ 5 js intro/js/kintamieji.js
@@ -0,0 +1,53 @@
+const gimimoMetai = 1900;
+
+console.log(5);
+console.log(gimimoMetai);
+
+console.log(2 + 2);
+const pirmas = 2;
+const antras = 7;
+const suma = pirmas + antras;
+console.log(pirmas, antras, suma);
+
+const vardas = 'Chuck';
+console.log(vardas);
+const zodis1 = 'Labas';
+const zodis2 = 'rytas';
+
+console.log(zodis1, zodis2);
+const sakinys = zodis1 + ' ' + zodis2;
+console.log(sakinys);
+
+const kabutes1 = "tekstas su vienguba (') kabute.";
+console.log(kabutes1);
+
+const kabutes2 = 'Tekstas su dviguba (") kabute.';
+console.log(kabutes2);
+
+const kabutes12 = 'Tekstas su vienguba (\') ir su dviguba (") kabute.';
+console.log(kabutes12);
+
+const tusciasSarasas = [];
+console.log(tusciasSarasas);
+
+
+const pazymiai = 10, 2, 7, 3, 8];
+console.log(pazymiai);
+
+const miestai = ['Anyksciai', 'Birzai', 'Chikago'];
+console.log(miestai);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
